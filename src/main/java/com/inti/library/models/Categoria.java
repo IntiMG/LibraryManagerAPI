af65e378bee3ab6f6a82d3223c01826bb35210c9
IntiMG <intimontalvan@uamv.edu.ni>
2026-08-12 18:06:45 -0600

    feat: crear entidad Categoria

---
+++ src/main/java/com/inti/library/models/Categoria.java
@@ -0,0 +1,34 @@
+package com.inti.library.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categorias")
+@Getter @Setter
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
