fef5eb381fd47062f79e24567099a6e3755feaab
IntiMG <intimontalvan@uamv.edu.ni>
2026-08-12 18:59:23 -0600

    refactor: renombrar paquete repository a repositories por consistencia

--- src/main/java/com/inti/library/repositories/CategoriaRepository.java
+++ src/main/java/com/inti/library/repositories/CategoriaRepository.java
@@ -1,4 +1,4 @@
-package com.inti.library.repository;
+package com.inti.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +7,5 @@ import com.inti.library.models.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    
+
 }
--- src/main/java/com/inti/library/repositories/LibroRepository.java
+++ src/main/java/com/inti/library/repositories/LibroRepository.java
@@ -1,4 +1,4 @@
-package com.inti.library.repository;
+package com.inti.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
